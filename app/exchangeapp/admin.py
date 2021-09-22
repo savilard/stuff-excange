2d42637fb974849bb65a4bf889f0d3106c7994a1
savilard <vitaliyantonoff@gmail.com>
2021-09-22 18:09:49 +0600

    feat(excahgeapp): add Product and ProductCategory to  admin.py

--- app/exchangeapp/admin.py
+++ app/exchangeapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+from .models import ProductCategory
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'slug',
+        'category',
+        'created_at',
+        'published',
+    ]
+
+    list_per_page = 25
