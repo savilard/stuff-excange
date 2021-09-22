2f6ea479681e301af556b8786141a14fc7f8b3ef
Vitaliy Antonov <60862933+savilard@users.noreply.github.com>
2021-09-22 17:20:33 +0600

    feat(excahgeapp): create exchangeapp (#4)

--- app/stuff_exchange/settings.py
+++ app/stuff_exchange/settings.py
@@ -15,6 +15,7 @@ DEBUG = env.bool('DEBUG', True)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
+    'exchangeapp.apps.ExchangeappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
