14dd8c30118068da15bbc7644a1bafcdc07d270c
savilard <vitaliyantonoff@gmail.com>
2021-09-22 17:46:19 +0600

    feat(excahgeapp): add Product and ProductCategory models

--- app/exchangeapp/models.py
+++ app/exchangeapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=50, db_index=True)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=50, db_index=True)
+    slug = models.SlugField(verbose_name='Product slug', max_length=100, unique=True)
+    category = models.ForeignKey(
+        ProductCategory,
+        verbose_name='Категория',
+        related_name='products',
+        on_delete=models.SET_NULL,
+    )
+    created_at = models.DateTimeField('Дата регистрации', default=timezone.now, db_index=True)
+    published = models.BooleanField('Опубликован', default=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+    def __str__(self):
+        return self.title
