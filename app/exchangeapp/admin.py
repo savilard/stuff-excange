4262e1ea48bfbf0d4ba2e52314b69786b3e28e58
Medet Mussayev <acostyle.m.z@protonmail.com>
2021-09-22 19:07:49 +0600

    feature(exchangeapp): add actions and search_field to admin.py

--- app/exchangeapp/admin.py
+++ app/exchangeapp/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin.decorators import action
+from django.utils.translation import ngettext
 
 from .models import Product
 from .models import ProductCategory
@@ -20,5 +22,27 @@ class ProductAdmin(admin.ModelAdmin):
         'created_at',
         'published',
     ]
+    list_filter = ('category', 'created_at', 'published')
+    search_fields = ("title__startswith", )
 
     list_per_page = 25
+
+    actions = ['make_published', 'make_unpublished']
+
+    @admin.action(description='Пометить выбранные товары как опубликованные')
+    def make_published(self, request, queryset):
+        updated = queryset.update(published=True)
+        self.message_user(request, ngettext(
+                '%d товар был успешно отмечен как опубликованный.',
+                '%d товара были успешно отмечены как опубликованные.',
+                updated,
+            ) % updated, messages.SUCCESS)
+
+    @admin.action(description='Пометить выбранные товары как неопубликованные')
+    def make_unpublished(self, request, queryset):
+        updated = queryset.update(published=False)
+        self.message_user(request, ngettext(
+                '%d товар был успешно отмечен как неопубликованный.',
+                '%d товара были успешно отмечены как неопубликованные.',
+                updated,
+            ) % updated, messages.SUCCESS)
