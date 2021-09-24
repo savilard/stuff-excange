cfaa870dd17861ad078290becc55f2742bebb14e
Medet <56687178+acostyle@users.noreply.github.com>
2021-09-24 12:50:11 +0600

    template(exchangeapp): add template to display all products (#14)
    * feat(exchangeapp): add all products template
    
    * style(exchangeapp): fixing the formatting of all_products template
    
    * style(exchangeapp): add view to name property in path

--- app/exchangeapp/views.py
+++ app/exchangeapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def add_product(request):
     return render(request, 'add_product.html')
+
+
+def all_products(request):
+    return render(request, 'all_products.html')
