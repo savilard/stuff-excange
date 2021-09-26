afb5f23ffa228650cb049a41be8e3064ae362e13
Vitaliy Antonov <60862933+savilard@users.noreply.github.com>
2021-09-26 16:43:43 +0600

    feat(exchangeapp): add showing user productts (#26)

--- app/exchangeapp/urls.py
+++ app/exchangeapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('add_product/', views.ProductAddView.as_view(), name="AddProductView"),
     path('all_products/', views.all_products, name="AllProductsView"),
+    path('<str:username>/products/', views.UserProductsView.as_view(), name='user-products-list'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name="product-detail"),
 ]
