071ad7cb7672ef12e5c8fb7157262e81bf697936
savilard <vitaliyantonoff@gmail.com>
2021-09-26 17:35:39 +0600

    refactor(exchangeapp): make getting all products as ListView

--- app/exchangeapp/urls.py
+++ app/exchangeapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('add_product/', views.ProductAddView.as_view(), name="AddProductView"),
-    path('all_products/', views.all_products, name="AllProductsView"),
+    path('all_products/', views.ProductsDisplayAllView.as_view(), name="display-all-products"),
     path('<str:username>/products/', views.UserProductsView.as_view(), name='user-products-list'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name="product-detail"),
 ]
