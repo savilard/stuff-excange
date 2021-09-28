05975d59d7023017cf73e74d964803d4f74765d0
Vitaliy Antonov <60862933+savilard@users.noreply.github.com>
2021-09-28 14:59:54 +0600

    feat(exchangeapp): customize signup forms (#40)

---
+++ app/user_profile/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from allauth.account.forms import SignupForm
+
+
+class StuffExchangeSignupForm(SignupForm):
+    phonenumber = forms.CharField()
+    vk_link = forms.CharField(required=False)
+    telegram_link = forms.CharField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def custom_signup(self, request, user):
+        user.vk_link = self.cleaned_data['vk_link']
+        user.telegram_link = self.cleaned_data['telegram_link']
+        user.phonenumber = self.cleaned_data['phonenumber']
+        user.save()
